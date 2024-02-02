2f7e865af15dde6b9efac99c960251af01963b01
Paul M Shumate <+>
2024-02-01 21:40:32 -0600

    complete lab

--- index.js
+++ index.js
@@ -3,25 +3,31 @@ const element = document.createElement("div");
 document.body.append(element);
 
 const ul = document.createElement("ul");
+
 for (let i = 0; i < 3; i++) {
   const li = document.createElement("li");
   li.textContent = (i + 1).toString();
   ul.append(li);
 }
+
 element.append(ul);
+
+const mainElement = document.getElementById("main");
+mainElement.style.height = "300px";
+mainElement.style.backgroundColor = "#27647B";
+mainElement.textContent = "You've changed what's on the screen!";
+mainElement.style.fontSize = "24px";
+mainElement.style.marginLeft = "30px";
+mainElement.style.lineHeight = 2;
+mainElement.classList.add("pet-listing", "dog");
+
+const secondChild = ul.querySelector("li:nth-child(2)");
+ul.removeChild(secondChild);
+
+ul.remove();
 
 const main = document.getElementById("main");
 main.innerHTML =
   "<h1>Poodles!</h1><h3>An Essay into the Pom-Pom as Aesthetic Reconfiguration of the Other from a post-Frankfurt School Appropriationist Perspective</h3><p><em>By: Byron Q. Poodle, Esq., BA.</em></p>";
 
-const elementToChange = document.getElementById("main");
-elementToChange.style.height = "300px";
-elementToChange.style.backgroundColor = "#27647B";
-elementToChange.textContent = "You've changed what's on the screen!";
-elementToChange.style.fontSize = "24px";
-elementToChange.style.marginLeft = "30px";
-elementToChange.style.lineHeight = 2;
-
-const ulToRemove = document.querySelector("ul");
-const secondChild = ulToRemove.querySelector("li:nth-child(2)");
-ulToRemove.removeChild(secondChild);
+main.textContent = "YOUR-NAME is the champion";
